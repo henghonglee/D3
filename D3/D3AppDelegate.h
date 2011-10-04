d785970103045cd70c471fc1420791cf0728b259
henghonglee <henghong.lee@gmail.com>
2011-10-05 00:04:11 +0800

    Initial Commit

---
+++ D3/D3AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  D3AppDelegate.h
+//  D3
+//
+//  Created by Shaun Tan on 5/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface D3AppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ D3/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  D3
+//
+//  Created by Shaun Tan on 5/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
